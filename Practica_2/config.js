0a211d62abeeda741f8bb273a8c9697c0ad37698
Patricia Ortiz Zamora <32482325+patrioz@users.noreply.github.com>
2018-01-13 19:55:31 +0100

    Daos hechos y dependencias, certificados y demas metidos.
    DAOS hechos.

---
+++ Practica_2/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    mysqlConfig: {
+
+        database: "Practica2_aw",
+        host: "localhost",
+        user: "root",
+        password: ""
+    },
+    
+    port: 3000
+};
