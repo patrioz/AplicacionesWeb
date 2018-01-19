dec393046b55d2889678f59d46a45f6466adf457
Patricia Ortiz Zamora <32482325+patrioz@users.noreply.github.com>
2018-01-19 16:50:33 +0100

    practica 2

---
+++ Practica_2/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    mysqlConfig: {
+
+        database: "practica2_aw",
+        host: "localhost",
+        user: "root",
+        password: ""
+    },
+    
+    port: 3000
+};
---
+++ Practica_2/public/js/index.js
@@ -0,0 +1,86 @@
+"use strict"
+
+//variables de mi sesion
+ let login;
+ let loginId;
+ let cadena;
+
+ $(document).ready(() =>{
+
+    $("#entrar").on("click", inicio);
+    $("#nuevo").on("click",registrarse);
+ });
+
+
+
+function inicio(){
+
+    let usuario = $("#usu").val();
+    let contaseña = $("#pass").val();
+
+    $.ajax({
+        type: "POST",
+            url:"/iniciarSesion",
+            contentType:"application/json",
+            data:JSON.stringify({
+                usuario:usuario,
+                contaseña: contraseña
+            }),
+
+            //funcion entrar en app
+
+            success: (data,textStatus, jqXHR)=>{ 
+
+                login = data.nombre;
+                loginId= data.id;
+                cadena = btoa(usuario + ":" + contraseña);
+
+
+                $("#log").hide();
+            },
+
+            //En caso de error, mostramos el error producido
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert("Se ha producido un error: " + errorThrown);
+            }
+
+
+    });
+
+}//Inicio
+
+
+function registrarse(){
+
+    let usuario = $("#usu").val();
+    let contaseña = $("#pass").val();
+    
+
+    $.ajax({
+        
+        type: "POST",
+            url:"/registrarUsuario",
+            contentType:"application/json",
+            data:JSON.stringify({
+                usuario:usuario,
+                contaseña: contraseña,
+            }),
+
+            //funcion entrar en app
+
+            success: (data,textStatus, jqXHR)=>{ 
+                
+
+                $("#log").after("Usuario " + data.nombre );
+            },
+
+            //En caso de error, mostramos el error producido
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert("Se ha producido un error: " + errorThrown);
+            }
+
+
+    });  
+}//Registrarse
+
+ 
